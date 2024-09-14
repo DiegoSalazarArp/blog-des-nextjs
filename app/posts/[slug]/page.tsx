bd1656e89de39475e432c6e94632ce8dcab57978
DiegoSalazar <d.salazararp@gmail.com>
2024-09-14 16:27:42 -0300

    metadata

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { myPortableImageComponent } from "@/sanity/utils/function";
 import BadgeComponent from "@/components/blog/BadgeComponent";
 import { PostSanityDocument } from "@/app/types/global";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Head from "next/head";
 
 const POST_QUERY = `
   *[_type == "post" && slug.current == $slug][0]{
@@ -54,59 +55,73 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const avatarNamePrefix = post.author.name.split(" ")[0].charAt(0);
 
   return (
-    <div>
-      <article className="border shadow bg-background">
+    <>
+      <Head>
+        <title>{post.title}</title>
+        {/* <meta name="description" content={post.description || 'Descripción por defecto'}> */}
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.description || 'Descripción por defecto'} />
+        <meta property="og:image" content={postImageUrl} />
+        <meta property="og:url" content={`https://tusitio.com/blog/${post.slug.current}`} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={post.title} />
+        <meta property="twitter:description" content={post.description || 'Descripción por defecto'} />
+        <meta property="twitter:image" content={postImageUrl} />
+      </Head>
+      <div>
+        <article className="border shadow bg-background">
 
-        <div className="sm:py-8 md:px-6 lg:px-8">
-          <div className="max-w-3xl mx-auto ">
-            <Image
-              src={postImageUrl || "https://via.placeholder.com/550x310"}
-              width={1200}
-              height={600}
-              alt="Blog Post Hero"
-              className="object-fill aspect-[2/1]"
-            />
-            <div className="mt-8 px-4 space-y-4">
-              <h1 className="text-4xl font-bold tracking-tight">{post.title}</h1>
-              <Link href={`/author/${post.author.slug}`}>
-                <div className="mt-4 flex items-center space-x-2  text-muted-foreground hover:underline">
-                  <div className=" flex items-center ">
-                    <Avatar className="h-14 w-14">
-                      <AvatarImage src={avatarImageUrl} alt="" />
-                      <AvatarFallback>{avatarNamePrefix}</AvatarFallback>
-                    </Avatar>
-                  </div>
-                  <div className="flex flex-col pl-6 ">
-                    <p className="font-medium italic">{post.author.name}</p>
-                    <p className="text-sm">
-                      Published on {new Date(post.publishedAt!).toLocaleDateString('en-US', {
-                        month: '2-digit',
-                        day: '2-digit',
-                        year: 'numeric',
-                        hour: '2-digit',
-                        minute: '2-digit',
+          <div className="sm:py-8 md:px-6 lg:px-8">
+            <div className="max-w-3xl mx-auto ">
+              <Image
+                src={postImageUrl || "https://via.placeholder.com/550x310"}
+                width={1200}
+                height={600}
+                alt="Blog Post Hero"
+                className="object-fill aspect-[2/1]"
+              />
+              <div className="mt-8 px-4 space-y-4">
+                <h1 className="text-4xl font-bold tracking-tight">{post.title}</h1>
+                <Link href={`/author/${post.author.slug}`}>
+                  <div className="mt-4 flex items-center space-x-2  text-muted-foreground hover:underline">
+                    <div className=" flex items-center ">
+                      <Avatar className="h-14 w-14">
+                        <AvatarImage src={avatarImageUrl} alt="" />
+                        <AvatarFallback>{avatarNamePrefix}</AvatarFallback>
+                      </Avatar>
+                    </div>
+                    <div className="flex flex-col pl-6 ">
+                      <p className="font-medium italic">{post.author.name}</p>
+                      <p className="text-sm">
+                        Published on {new Date(post.publishedAt!).toLocaleDateString('en-US', {
+                          month: '2-digit',
+                          day: '2-digit',
+                          year: 'numeric',
+                          hour: '2-digit',
+                          minute: '2-digit',
 
-                      })}
-                    </p>
+                        })}
+                      </p>
+                    </div>
                   </div>
+                </Link>
+                {/* validar primero si existen y luego hacer el map para evitar errores de renderizado */}
+                {post.categories && post.categories.map((category, i) => {
+                  return <BadgeComponent key={i} categoryTitle={category.title} />;
+                })}
+                <div>
                 </div>
-              </Link>
-              {/* validar primero si existen y luego hacer el map para evitar errores de renderizado */}
-              {post.categories && post.categories.map((category, i) => {
-                return <BadgeComponent key={i} categoryTitle={category.title} />;
-              })}
-              <div>
+              </div>
+              <div className="mx-auto px-4 text-justify tracking-normal prose prose-lg mt-8">
+                <PortableText
+                  value={post.body}
+                  components={myPortableImageComponent}
+                />
               </div>
             </div>
-            <div className="mx-auto px-4 text-justify tracking-normal prose prose-lg mt-8">
-              <PortableText
-                value={post.body}
-                components={myPortableImageComponent}
-              />
-            </div>
           </div>
-        </div>
-      </article>
-    </div>
+        </article>
+      </div>
+    </>
   );
 }
